094671001ad407aa7533e9abd1fe4f1c518b837e
Paulo Freitas <paulofreitas.dev@gmail.com>
2025-11-26 08:18:23 -0300

    creating A.I slime

--- RPG-2D/Assets/Scripts/Actors/Enemies/Slime/Slime.cs
+++ RPG-2D/Assets/Scripts/Actors/Enemies/Slime/Slime.cs
@@ -1,29 +1,62 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Slime : MonoBehaviour
 {
     [Header("Components")]
+    private Animator anim;
     private Rigidbody2D rig;
 
     [Header("Stats")]
     [SerializeField] private float health;
     [SerializeField] private float speed;
+    [SerializeField] private float initialSpeed;
+    [SerializeField] private int index;
+
+    public List<Transform> paths = new List<Transform>();
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        initialSpeed = speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        OnMove();
     }
 
     void OnMove()
     {
+        transform.position = Vector2.MoveTowards(transform.position, paths[index].position, speed * Time.deltaTime);
 
+        if (Vector2.Distance(transform.position, paths[index].position) < 0.1)
+        {
+            if (index < paths.Count - 1)
+            {
+                //index++;
+                index = Random.Range(0, paths.Count - 1);
+            }
+
+            else
+            {
+                index = 0;
+            }
+        }
+
+        Vector2 direction = paths[index].position - transform.position;
+
+        if (direction.x > 0)
+        {
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+
+        if (direction.x < 0)
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+        }
     }
 
     void OnAttack()
